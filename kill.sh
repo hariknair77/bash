741c7e1a312935c99e4c237ba27721bd7cefcd60
hari <hariknair77@gmail.com>
2019-04-07 11:36:17 +0530

    kill extra features

--- kill.sh
+++ kill.sh
@@ -3,10 +3,15 @@ echo -n "Enter cpu limit : "
 read limit
 ps -eo pid,%cpu --sort=-%cpu > pros.txt
 awk -v limit="$limit" ' $2 > limit {print $1}' pros.txt > kill.txt
-echo "prosses exceed the cpu limit are..."
-for i in $(cat kill.txt);do
-	echo $(ps -p $i -o comm= )
-done
+if [ ! -s kill.txt ];then
+	echo "No prosses exceed the limit"
+else
+	echo "prosses exceed the cpu limit are..."
+	echo -e "COMMAND\t %CPU"
+	for i in $(cat kill.txt);do
+		echo $(ps -p $i -o comm=,%cpu= )
+	done
+fi
 
 for i in $(cat kill.txt);do
 	echo -en "\ndo you want to kill $(ps -p $i -o comm= ) [y/n] "
